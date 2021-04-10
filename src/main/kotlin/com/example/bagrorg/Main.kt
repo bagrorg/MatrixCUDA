7432639916336304218544a27aaafcac3778c3bd
bagrorg <kuldanvit@gmail.com>
2021-04-11 00:21:26 +0300

    +corrected main

--- src/main/kotlin/com/example/bagrorg/Main.kt
+++ src/main/kotlin/com/example/bagrorg/Main.kt
@@ -1,9 +1,12 @@
 package com.example.bagrorg
 
 fun main() {
-    var vector = Vector(5, IntArray(5){ 0 })
-    var vector2 = Vector(5, IntArray(5){ it + 1 })
-    var vector3 = Vector(5, IntArray(5){ it + 1 })
+    var vector = Vector(5000, IntArray(5000){ 0 })
+    var vector2 = Vector(5000, IntArray(5000){ it + 1 })
+    var vector3 = Vector(5000, IntArray(5000){ it + 1 })
     GPUMatrixOperations.add(vector2, vector3)
+    println(vector)
+
+    val summ = GPUMatrixOperations.sum(vector)
     println(vector)
 }
